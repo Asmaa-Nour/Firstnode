f0f06fafe306daf8dba2e65444243b88bc2f6439
Asmaa Nour <asmaamo2so95@gmail.com>
2018-04-21 17:51:38 +0200

    adding new files

--- models/dbintializer.js
+++ models/dbintializer.js
@@ -1,2 +1,7 @@
 const mongoose=require('mongoose');
-mongoose.connect('mongodb://localhost:27017/expressProject');
+let mongo_url='mongodb://localhost:27017/express-project';
+if(process.env.NODE_ENV==='production'){
+    mongo_url='mongodb://asmaa:asmaa@ds111798.mlab.com:11798/express-project';
+}
+mongoose.connect();
+//'mongodb://localhost:27017/express-project'
