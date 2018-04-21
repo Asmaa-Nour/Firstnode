50cd98203ca14a52707a984ca83715192d26e305
Asmaa Nour <asmaamo2so95@gmail.com>
2018-04-21 18:08:06 +0200

    adding new files

--- models/dbintializer.js
+++ models/dbintializer.js
@@ -3,5 +3,5 @@ let mongo_url='mongodb://localhost:27017/express-project';
 if(process.env.NODE_ENV==='production'){
     mongo_url='mongodb://asmaa:asmaa@ds111798.mlab.com:11798/express-project';
 }
-mongoose.connect();
+mongoose.connect(mongo_url);
 //'mongodb://localhost:27017/express-project'
